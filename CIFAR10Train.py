e5cbfadbb4f5900c21ec76053a8177ee5b635359
codeocean <1264044626@qq.com>
2020-03-18 14:11:09 +0800

    lenet

--- CIFAR10Train.py
+++ CIFAR10Train.py
@@ -1,39 +1,49 @@
-from pathlib import Path
-
-import torch
 import torch.optim as optim
 import torchvision
 import torchvision.transforms as transforms
 
 from models import *
+import os
 
-transform = transforms.Compose(
-    [transforms.ToTensor(),
-     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+transform_train = transforms.Compose([
+    transforms.RandomCrop(32, padding=4),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
+
+transform_test = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
 
 batch_size = 128
 trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
-                                        download=True, transform=transform)
+                                        download=True, transform=transform_train)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
                                           shuffle=True, num_workers=2)
 testset = torchvision.datasets.CIFAR10(root='./data', train=False,
-                                       download=True, transform=transform)
+                                       download=True, transform=transform_test)
 testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
                                          shuffle=False, num_workers=2)
 
 classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
-PATH = './LeNet.pth'
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-net = LeNet()
+
+# net = LeNet()
+net = VGG('VGG16')
+
 net.to(device)
+PATH = './%s.pth' % net.name
+if os.path.exists(PATH):
+    net.load_state_dict(torch.load(PATH))
 
 
 def train():
-    if Path(PATH).exists():
-        net.load_state_dict(torch.load(PATH))
+    net.train()
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9, weight_decay=5e-4)
 
     for epoch in range(1000):  # loop over the dataset multiple times
 
@@ -55,8 +65,9 @@ def train():
 
             # print statistics
             running_loss += loss.item()
+
             if i % 100 == 99:  # print every 2000 mini-batches
-                print('[%d, %5d] loss: %.3f' % (epoch + 1, i + 1, running_loss / 100))
+                print('[%d, %5d] loss: %.5f' % (epoch + 1, i + 1, running_loss / 100))
                 running_loss = 0.0
 
         print('epoch:%d' % epoch)
@@ -68,6 +79,7 @@ def train():
 def test():
     correct = 0
     total = 0
+    net.eval()
     with torch.no_grad():
         for data in testloader:
             images, labels = data[0].to(device), data[1].to(device)
